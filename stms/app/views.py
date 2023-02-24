3059fbeea169a48b589b8c471fe048de560a1c4e
Hasnain <mrajhasnain101@gmail.com>
2023-02-24 17:01:36 +0600

    views

--- stms/app/views.py
+++ stms/app/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.forms import inlineformset_factory
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
 def home(request):
     return render(request,'app/index.html')
 
+def register(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        user=User.objects.create_user(username=name,password=password1,email=email,)
+        user.save()
+        return redirect('/')
+    else:
+        return render(request,'index.html')
 
 
 
