816144a21eb2342a43eae154757e745db20228c7
Hasnain <mrajhasnain101@gmail.com>
2023-02-24 17:08:27 +0600

    more and more files were addded

--- stms/app/models.py
+++ stms/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+class Register(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    password = models.CharField(max_length=30)
+
